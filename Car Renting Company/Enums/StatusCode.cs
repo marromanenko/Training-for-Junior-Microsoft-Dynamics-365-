6d5d1bc2e152fe4d5167f3b4442f41f2cb1b10fe
Mariia <88029788+marromanenko@users.noreply.github.com>
2022-02-21 10:52:05 +0200

    Add files via upload

---
+++ Car Renting Company/Enums/StatusCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Enums
+{
+    public enum StatusCode
+    {
+        Created = 315890003, 
+        Confirmed = 315890000,
+        Renting = 315890001,
+        Returned = 315890004,
+        Canceled = 315890002
+    }
+}
